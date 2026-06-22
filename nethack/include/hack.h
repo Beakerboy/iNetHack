187808d02c7c28f06cdaa7fed3885377a07d7ae0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-22 14:50:12 -0400

    Update hack.h

--- nethack/include/hack.h
+++ nethack/include/hack.h
@@ -1,7 +1,1 @@
-/* menu return list */
-typedef struct mi {
-    anything item;     /* identifier */
-    long count;        /* count */
-    unsigned itemflags; /* item flags */
-} menu_item;
-#define MENU_ITEM_P struct mi
+#include "wintype.h"

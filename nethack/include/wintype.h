e07f78dcfcb2c00b64b2794dcf8f77e254763648
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-22 15:05:35 -0400

    Update wintype.h

--- nethack/include/wintype.h
+++ nethack/include/wintype.h
@@ -5,3 +5,33 @@ typedef struct mi {
     unsigned itemflags; /* item flags */
 } menu_item;
 #define MENU_ITEM_P struct mi
+
+/* generic parameter - must not be any larger than a pointer */
+typedef union any {
+    genericptr_t a_void;
+    struct obj *a_obj;
+    struct monst *a_monst;
+    int a_int;
+    int a_xint16;
+    int a_xint8;
+    char a_char;
+    schar a_schar;
+    uchar a_uchar;
+    unsigned int a_uint;
+    long a_long;
+    unsigned long a_ulong;
+    coordxy a_coordxy;
+    int *a_iptr;
+    xint16 *a_xint16ptr;
+    xint8 *a_xint8ptr;
+    long *a_lptr;
+    coordxy *a_coordxyptr;
+    unsigned long *a_ulptr;
+    unsigned *a_uptr;
+    const char *a_string;
+    int (*a_nfunc)(void);
+    unsigned long a_mask32; /* used by status highlighting */
+    int64 a_int64;
+    uint64 a_uint64;
+    /* add types as needed */
+} anything;

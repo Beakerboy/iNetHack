492a0501e8f0f8098d73a1c6de919a5a9da1918b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-22 12:18:00 -0400

    Update NethackMenuItem.h

--- Classes/NethackMenuItem.h
+++ Classes/NethackMenuItem.h
@@ -21,7 +21,7 @@
 //  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
 
 #import <Foundation/Foundation.h>
-#include "hack.h"
+// #include "hack.h"
 
 @interface NethackMenuItem : NSObject
 

1c8465f483ec2aec2afd42564583e63ece06ab38
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 15:30:00 -0400

    Update dungeon.h

--- nethack/include/dungeon.h
+++ nethack/include/dungeon.h
@@ -2,3 +2,4 @@ typedef struct d_level { /* basic dungeon level element */
     xint16 dnum;          /* dungeon number */
     xint16 dlevel;        /* level number */
 } d_level;
+#define Is_rogue_level(x)   (Lcheck(x, &rogue_level))

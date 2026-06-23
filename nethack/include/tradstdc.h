1e21cfe5949284a2563a187b1b71ccf1ba185e71
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 10:48:17 -0400

    Delete nethack/include/tradstdc.h

--- nethack/include/tradstdc.h
+++
@@ -1,5 +0,0 @@
-/* generic pointer, always a macro; genericptr_t is usually a typedef */
-#define genericptr void *
-#ifndef genericptr_t
-typedef genericptr genericptr_t; /* (void *) or (char *) */
-#endif

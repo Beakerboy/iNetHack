8a18fe8954cd2d437e405429b2ba4d7b3ffa2caf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 14:58:28 -0400

    Update hack.h

--- nethack/include/hack.h
+++ nethack/include/hack.h
@@ -1,8 +1,9 @@
 #ifndef HACK_H
 #define HACK_H
 #include "config.h"
-#include "flag.h"
 #include "wintype.h"
+#include "flag.h"
+#include "display.h"
 #include "you.h"
 #include "extern.h"
 #include "decl.h"

662667ce8409bb963958b7e8cd315f267662f7ac
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 09:10:01 -0400

    Add typedefs for xint8 and xint16 types

--- nethack/include/global.h
+++ nethack/include/global.h
@@ -1,3 +1,10 @@
+
+/*
+ * type xint8: small integers (typedef'd as signed,
+ * in the range -127 - 127).
+ */
+typedef int8_t xint8;
+
 /*
  * type coordxy: integers (typedef'd as signed,
  * in the range -32768 to 32767), mostly coordinates.
@@ -5,3 +12,9 @@
  * surpass an upper limit of 127.
  */
 typedef int16_t coordxy;
+
+/*
+ * type xint16: integers (typedef'd as signed,
+ * in the range -32768 to 32767), non-coordinates.
+ */
+typedef int16_t xint16;

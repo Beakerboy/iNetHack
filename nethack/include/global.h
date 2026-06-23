a0309433e0756f7d84bb02deb66c3cd9b17ca4f8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 08:58:54 -0400

    Create global.h

---
+++ nethack/include/global.h
@@ -0,0 +1,7 @@
+/*
+ * type coordxy: integers (typedef'd as signed,
+ * in the range -32768 to 32767), mostly coordinates.
+ * Note that in 2022, screen coordinates easily
+ * surpass an upper limit of 127.
+ */
+typedef int16_t coordxy;

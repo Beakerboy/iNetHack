aee63ba9d1d4a2236565228b49b29ce7357f348a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 09:08:16 -0400

    Define uchar type for unsigned char compatibility
    Added typedef for uchar to handle unsigned char type.

--- nethack/include/config.h
+++ nethack/include/config.h
@@ -6,4 +6,15 @@
 typedef signed char schar;
 #endif
 
+/*
+ * type uchar:
+ * small unsigned integers (8 bits suffice - but 7 bits do not)
+ *      typedef unsigned char uchar;
+ * will be satisfactory if you have an "unsigned char" type; otherwise use
+ *      typedef unsigned short int uchar;
+ */
+#ifndef _AIX32 /* identical typedef in system file causes trouble */
+typedef unsigned char uchar;
+#endif
+
 #include "global.h"

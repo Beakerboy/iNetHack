814770c2224007bcca4a7e408a6d2186e8dac01f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 11:10:36 -0400

    Update you.h

--- nethack/include/you.h
+++ nethack/include/you.h
@@ -1,6 +1,11 @@
 #define ROLE_ALIGNS 3     /* number of permitted player alignments */
+/*** Unified structure specifying gender information ***/
+struct Gender {
+    const char *adj;      /* male/female/neuter */
+};
 #define ROLE_GENDERS 2    /* number of permitted player genders
                              increment to 3 if you allow neuter roles */
+extern const struct Gender genders[]; /* table of available genders */
 struct Race {
     /*** Strings that name various things ***/
     const char *noun;           /* noun ("human", "elf") */

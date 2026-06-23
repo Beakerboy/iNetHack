82b7b700bf1fda1a3b3977de75c79cfff8b4060b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 11:02:24 -0400

    Define permitted player genders in you.h
    Added a definition for permitted player genders.

--- nethack/include/you.h
+++ nethack/include/you.h
@@ -1,1 +1,3 @@
 #define ROLE_ALIGNS 3     /* number of permitted player alignments */
+#define ROLE_GENDERS 2    /* number of permitted player genders
+                             increment to 3 if you allow neuter roles */

c34d9c84a302bcdf065baacaf7e6f7d4c464bd56
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 10:21:44 -0400

    Update integer.h

--- nethack/include/integer.h
+++ nethack/include/integer.h
@@ -5,8 +5,6 @@ typedef signed char int8_t;
 typedef long long int int64_t;
 typedef unsigned long long int uint64_t;
 #endif
-typedef uint8_t uint8;
-typedef unsigned char uint8_t;
 typedef short int16_t;
 typedef int64_t int64;
 typedef uint64_t uint64;

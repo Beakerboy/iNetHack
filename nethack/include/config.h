6d0cfddcd8ed769d8f7845beffb4aa33434c6deb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 09:45:29 -0400

    Update config.h

--- nethack/include/config.h
+++ nethack/include/config.h
@@ -19,3 +19,10 @@ typedef unsigned char uchar;
 
 #include "integer.h"
 #include "global.h"
+
+/*
+ *      Defining INSURANCE slows down level changes, but allows games that
+ *      died due to program or system crashes to be resumed from the point
+ *      of the last level change, after running a utility program.
+ */
+#define INSURANCE /* allow crashed game recovery */

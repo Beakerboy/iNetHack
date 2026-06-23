e0514afd9f8bc5dd9d75d1d302ed2e13669bce02
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 16:18:16 -0400

    Adjust glyph offsets for female and pet glyphs

--- nethack/include/display.h
+++ nethack/include/display.h
@@ -2,17 +2,17 @@
 enum glyph_offsets {
     GLYPH_MON_OFF = 0,
     GLYPH_MON_MALE_OFF = (GLYPH_MON_OFF),
-    GLYPH_MON_FEM_OFF = (NUMMONS + GLYPH_MON_MALE_OFF),
-    GLYPH_PET_OFF = (NUMMONS + GLYPH_MON_FEM_OFF),
+    GLYPH_MON_FEM_OFF = (5 + GLYPH_MON_MALE_OFF),
+    GLYPH_PET_OFF = (5S + GLYPH_MON_FEM_OFF),
     GLYPH_PET_MALE_OFF = (GLYPH_PET_OFF),
-    GLYPH_PET_FEM_OFF = (NUMMONS + GLYPH_PET_MALE_OFF),
+    GLYPH_PET_FEM_OFF = (5 + GLYPH_PET_MALE_OFF),
     MAX_GLYPH
 };
 #define NO_GLYPH          MAX_GLYPH
 #define glyph_is_female_pet(glyph) \
-    ((glyph) >= GLYPH_PET_FEM_OFF && (glyph) < (GLYPH_PET_FEM_OFF + NUMMONS))
+    ((glyph) >= GLYPH_PET_FEM_OFF && (glyph) < (GLYPH_PET_FEM_OFF + 5))
 #define glyph_is_male_pet(glyph) \
     ((glyph) >= GLYPH_PET_MALE_OFF                      \
-     && (glyph) < (GLYPH_PET_MALE_OFF + NUMMONS))
+     && (glyph) < (GLYPH_PET_MALE_OFF + 5))
 #define glyph_is_pet(glyph) \
     (glyph_is_male_pet(glyph) || glyph_is_female_pet(glyph))

635b9c5fc46dd63b75d9daacb1fcc81c9cdc3810
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 15:45:34 -0400

    Remove uchar typedef from config.h
    Remove uchar typedef comment and definition for AIX32.

--- nethack/include/config.h
+++ nethack/include/config.h
@@ -1,14 +1,4 @@
 #include "tradstdc.h"
-/*
- * type uchar:
- * small unsigned integers (8 bits suffice - but 7 bits do not)
- *      typedef unsigned char uchar;
- * will be satisfactory if you have an "unsigned char" type; otherwise use
- *      typedef unsigned short int uchar;
- */
-#ifndef _AIX32 /* identical typedef in system file causes trouble */
-typedef unsigned char uchar;
-#endif
 
 #include "integer.h"
 #include "global.h"

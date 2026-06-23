bd5d6af136d5753e48a818003d3f395337e7b88d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 10:06:12 -0400

    Update integer.h

--- nethack/include/integer.h
+++ nethack/include/integer.h
@@ -1,2 +1,9 @@
+#if defined(__cplusplus)
+#include <stdint.h>
+#else
+typedef long long int int64_t;
+typedef unsigned long long int uint64_t;
+#endif
+
 typedef int64_t int64;
 typedef uint64_t uint64;

27ade5034b7bfba3f20c6c232c97ad1149e10116
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-23 15:31:01 -0400

    Update display.h

--- nethack/include/display.h
+++ nethack/include/display.h
@@ -1,3 +1,6 @@
 // Not really, just filling it in.
 #define MAX_GLYPH          10
 #define NO_GLYPH          MAX_GLYPH
+
+#define glyph_is_pet(glyph) \
+    (glyph_is_male_pet(glyph) || glyph_is_female_pet(glyph))

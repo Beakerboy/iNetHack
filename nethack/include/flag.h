4eedc0ed677e2db924d7dbe54062e4de9aa73272
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-24 12:07:54 -0400

    Update flag.h

--- nethack/include/flag.h
+++ nethack/include/flag.h
@@ -6,4 +6,11 @@ struct flag {
     int initgend;  /* starting gender    (index into genders[]) */
     int initalign; /* starting alignment (index into aligns[])  */
 };
+
+struct instance_flags {
+    boolean wc_color;         /* use color graphics                  */
+};
+#define use_color wc_color
+
 extern NEARDATA struct flag flags;
+extern NEARDATA struct instance_flags iflags;

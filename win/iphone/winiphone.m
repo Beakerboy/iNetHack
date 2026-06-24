57576845a47ca3c8fc8896aa859dd5a231edae3b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-24 11:34:14 -0400

    Update winiphone.m

--- win/iphone/winiphone.m
+++ win/iphone/winiphone.m
@@ -1,4 +1,32 @@
 #define kOptionWizard (@"wizard")
+@interface WinIPhone : NSObject {}
+
++ (void) triggerInitialize;
+
+@end
+
+@implementation WinIPhone
+
++ (void) triggerInitialize {
+	// do nothing
+}
+
++ (void) initialize {
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	[defaults registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys:
+								@"YES", kOptionAutopickup,
+								@"$\"=/!?+", kOptionPickupTypes,
+                                @"`", kOptionBoulderSym,
+                                @"YES", kOptionTravel,
+                                @"YES", kOptionPickupThrown,
+								@"YES", kOptionAutokick,
+								@"YES", kOptionShowExp,
+								@"YES", kOptionTime,
+								@"YES", kOptionAutoDig,
+								nil]];
+}
+
+@end
 
 #if TARGET_IPHONE_SIMULATOR
     wizard = YES; //iNethack2 YES for sim usually..

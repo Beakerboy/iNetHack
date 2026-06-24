d1a2f6443acea6f035581466d4e536ec68e2d8c3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-24 10:38:54 -0400

    Update func_tab.h

--- nethack/include/func_tab.h
+++ nethack/include/func_tab.h
@@ -1,3 +1,5 @@
+#ifndef FUNC_TAB_H
+#define FUNC_TAB_H
 #define AUTOCOMPLETE 0x0002 /* command autocompletes */
 #define WIZMODECMD   0x0004 /* wizard-mode command */
 #define CMD_NOT_AVAILABLE 0x0010 /* recognized but non-functional (!SHELL,&c)*/
@@ -11,3 +13,5 @@ struct ext_func_tab {
 };
 
 extern struct ext_func_tab extcmdlist[];
+
+#endif /* FUNC_TAB_H */

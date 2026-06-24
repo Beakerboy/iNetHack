622dd56b97aa723766c4016548f840d999c92dda
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-24 11:26:32 -0400

    Update winiphone.m

--- win/iphone/winiphone.m
+++ win/iphone/winiphone.m
@@ -1,1 +1,8 @@
 #define kOptionWizard (@"wizard")
+
+#if TARGET_IPHONE_SIMULATOR
+    wizard = YES; //iNethack2 YES for sim usually..
+#else */
+    wizard = [defaults boolForKey:kOptionWizard];
+
+#endif

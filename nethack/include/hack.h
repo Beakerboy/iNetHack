1cbe43199de5a3769ac6595a97c8c2e2555ae4a5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-24 09:11:01 -0400

    Update hack.h

--- nethack/include/hack.h
+++ nethack/include/hack.h
@@ -8,4 +8,12 @@
 #include "you.h"
 #include "extern.h"
 #include "decl.h"
+
+struct dgn_topology { /* special dungeon levels for speed */
+    d_level d_rogue_level;
+};
+
+// Not exactly copied
+#define rogue_level             (svd.dungeon_topology.d_rogue_level)
+
 #endif /* HACK_H */

322c9cbd263fe68bd9a08c3328399638b6613727
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-24 11:09:59 -0400

    Update tradstdc.h

--- nethack/include/tradstdc.h
+++ nethack/include/tradstdc.h
@@ -1,1 +1,28 @@
+#ifdef UNWIDENED_PROTOTYPES
+#define CHAR_P char
+#define SCHAR_P schar
+#define UCHAR_P uchar
+#define XCHAR_P coordxy
+#define SHORT_P short
+#ifndef SKIP_BOOLEAN
+#define BOOLEAN_P boolean
+#endif
+#define ALIGNTYP_P aligntyp
+#else
+#ifdef WIDENED_PROTOTYPES
+#define CHAR_P int
+#define SCHAR_P int
+#ifndef UCHAR_P
+#define UCHAR_P int
+#endif
+#define XCHAR_P int
+#define SHORT_P int
+#define BOOLEAN_P int
+#define ALIGNTYP_P int
+#else
+/* Neither widened nor unwidened prototypes.  Argument list expansion
+ * by VDECL always empty; all xxx_P vanish so defs aren't needed. */
+#endif
+#endif
+
 #define NONNULLARG12

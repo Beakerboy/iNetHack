584fdfd1fc61353d1428df2cc79f5bc592f5eb1b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-24 11:49:52 -0400

    Add debug flag to struct flag

--- nethack/include/flag.h
+++ nethack/include/flag.h
@@ -1,4 +1,6 @@
 struct flag {
+    boolean debug;           /* in debugging mode (aka wizard mode) */
+#define wizard flags.debug
     int initrole;  /* starting role      (index into roles[])   */
     int initrace;  /* starting race      (index into races[])   */
     int initgend;  /* starting gender    (index into genders[]) */

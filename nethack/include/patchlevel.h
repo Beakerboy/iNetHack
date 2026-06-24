5a8e67d4474031f763e37c342eb01258255fff94
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-24 09:27:21 -0400

    Update patchlevel.h

--- nethack/include/patchlevel.h
+++ nethack/include/patchlevel.h
@@ -1,1 +1,12 @@
-
+/* NetHack 5.0.x */
+#define VERSION_MAJOR 5
+#define VERSION_MINOR 0
+/*
+ * PATCHLEVEL is updated for each release.
+ */
+#define PATCHLEVEL 0
+/*
+ * Incrementing EDITLEVEL can be used to force invalidation of old bones
+ * and save files.
+ */
+#define EDITLEVEL 0

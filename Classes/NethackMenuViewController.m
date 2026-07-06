b3a7053e5f4e982d2066a32c127662b040828035
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-06 12:12:04 -0400

    Update NethackMenuViewController.m

--- Classes/NethackMenuViewController.m
+++ Classes/NethackMenuViewController.m
@@ -89,7 +89,7 @@ extern short glyph2tile[];
 		}
 		if (menuWindow.acceptMoney) {
 			any.a_int = '$';
-            NSString *title = [winiphone universalMoneyString];
+            NSString *title = [WinIPhone universalMoneyString];
 
 			NethackMenuItem *mi = [[NethackMenuItem alloc] initWithId:&any title:[title cStringUsingEncoding:NSASCIIStringEncoding]
 																glyph:kNoGlyph isMeta:YES preselected:NO];

cf726a003b95afbc4c9686cdb0a07181f9d83eeb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-07 17:53:28 -0400

    Implement NetHackEngine34 class methods

---
+++ nethack34/src/NetHackEngine34.m
@@ -0,0 +1,11 @@
+#import "NetHackEngine34.h"
+#import "hack.h" // Safe to import here inside the framework
+
+@implementation NetHackEngine34
+- (void)startGameWithSavePath:(NSString *)path {
+    // 3.4 specific code
+}
+- (void)sendInput:(NSString *)input {
+    // 3.4 specific code
+}
+@end

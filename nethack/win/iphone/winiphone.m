e98a9412cc48da7fe59cb65532e09072c5c58c60
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-07 14:55:11 -0400

    Update winiphone.m

--- nethack/win/iphone/winiphone.m
+++ nethack/win/iphone/winiphone.m
@@ -179,6 +179,27 @@ genl_can_suspend_no,
 								nil]];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	struct ext_func_tab *f = extcmdlist;
+    filteredExtCmd = [[NSMutableArray alloc] init];
+    filteredExtCmdIndex = [[NSMutableArray alloc] init];
+    int filtered = 0;
+    int row = 0;
+	while (f++->ef_txt) {
+        filtered++;
+        if (![self showExtCmd:row]) {
+            // Filter out items that shouldn't be displayed: wizard mode, regular commands, etc.
+            filtered--;
+        } else {
+            NSValue *value = [NSValue valueWithBytes:&extcmdlist[row] objCType:@encode(struct ext_func_tab)];
+            [filteredExtCmd addObject:value];
+            [filteredExtCmdIndex addObject:[NSNumber numberWithInt: row]];
+        }
+        row++;
+	}
+    return filtered;
+}
+
 /* should we display this extended command? */
 - (Boolean) showExtCmd:(int) row {
     struct ext_func_tab *efp = &extcmdlist[row];

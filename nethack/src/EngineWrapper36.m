fb3703a9821d4d1c75410ecf4cdc1bc6b7cd44d5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-08 18:45:47 -0400

    Assign global window delegate in setDelegate method
    Added global window delegate assignment in setDelegate method.

--- nethack/src/EngineWrapper36.m
+++ nethack/src/EngineWrapper36.m
@@ -1,7 +1,15 @@
 #import "EngineWrapper36.h"
 #import "hack.h"
 
+__weak id<NetHackEngineDelegate> _globalWindowDelegate = nil;
+
 @implementation EngineWrapper36
+
+- (void)setDelegate:(id<NetHackEngineDelegate>)delegate {
+    _delegate = delegate;
+    _globalWindowDelegate = delegate; // Assigns the global pointer
+}
+
 - (void)main {
     iphone_main();
 }

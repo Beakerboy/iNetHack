84ebeead70a75fae3d49e15cdfdaafe6a066d2a9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-08 14:57:29 -0400

    Delete Classes/NetHackEngineProtocol.h

--- Classes/NetHackEngineProtocol.h
+++
@@ -1,6 +0,0 @@
-// NetHackEngineProtocol.h
-@protocol NetHackEngineProtocol <NSObject>
-- (void)startGameWithSavePath:(NSString *)path;
-- (void)sendInput:(NSString *)input;
-// Add other lifecycle or UI callback hooks here
-@end

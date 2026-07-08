3d78aba009c7fe31c9b6ec0a4018bf4232e0d5f0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-08 15:16:23 -0400

    Create NetHackEngineProtocol.h

---
+++ Classes/NetHackEngineProtocol.h
@@ -0,0 +1,5 @@
+@protocol NetHackEngineProtocol <NSObject>
+- (void)startGameWithSavePath:(NSString *)path;
+- (void)sendInput:(NSString *)input;
+// Add other lifecycle or UI callback hooks here
+@end

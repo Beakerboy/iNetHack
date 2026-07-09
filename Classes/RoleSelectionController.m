e2c8a4b9bd9e5c13eccfd70254985904eb001fcb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-09 16:15:38 -0400

    Update RoleSelectionController.m

--- Classes/RoleSelectionController.m
+++ Classes/RoleSelectionController.m
@@ -12,6 +12,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
 
+#import "iNethackAppDelegate.h"
 #import "RoleSelectionController.h"
 #import "MenuItem.h"
 #import "MenuViewController.h"

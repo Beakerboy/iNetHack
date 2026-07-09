01fb56a62a7dcbd6ec9ec632285643a8fdf37fc6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-09 09:57:53 -0400

    Update EngineWrapper36.h

--- nethack/include/EngineWrapper36.h
+++ nethack/include/EngineWrapper36.h
@@ -5,10 +5,22 @@ typedef int winid;
 @protocol NetHackEngineDelegate <NSObject>
 - (winid)createWindowWithType:(int)type;
 - (void)destroyWindowWithId:(winid)wid;
-- (void)updateScreen;
-- (int)animFrame;
-- (void)setAnimFrame:(int)frame;
+- (void)displayFile:(NSString *)filename mustExist:(BOOL)e;
+- (void)displayMenuWindow:(Window *)w;
+- (void)displayWindowId:(winid)wid blocking:(BOOL)blocking;
+- (void)displayYnQuestion:(NethackYnFunction *)yn;
 - (void)doPlayerSelection;
+- (NethackEvent *)fetchNextInputEvent;
+- (char)getDirectionInput;
+- (int)getExtendedCommand;
+- (void)getLine:(char *)line prompt:(const char *)prompt;
+- (void)resetGlyphCache;
+- (void)setAnimFrame:(int)frame;
+- (void)showKeyboard:(BOOL)d;
+- (void)updateScreen;
+- (Window *)windowWithId:(winid)wid;
+
+- (int)animFrame;
 
 // ... add future migrated window functions here ...
 @end

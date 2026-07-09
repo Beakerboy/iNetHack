aafab019ce92e22424b082d97821499aa04ab1e1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-09 13:35:08 -0400

    Update EngineWrapper36.m

--- nethack/src/EngineWrapper36.m
+++ nethack/src/EngineWrapper36.m
@@ -10,7 +10,7 @@ __weak id<NetHackEngineDelegate> _globalWindowDelegate = nil;
     _globalWindowDelegate = delegate; // Assigns the global pointer
 }
 
-- (void)main {
+- (void)framework_main {
     main();
 }
 - (void)haptic_reset {

a6a95f1b28a1af114ca98b961cb74840395fdbed
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-10 13:39:49 -0400

    Update NethackMenuViewController.h

--- Classes/NethackMenuViewController.h
+++ Classes/NethackMenuViewController.h
@@ -21,7 +21,7 @@
 //  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
 
 #import <UIKit/UIKit.h>
-@import NetHackEngine36;
+
 #define kMenuCancelled (-1)
 
 @class Window, NethackMenuItem, ItemAmountViewController;

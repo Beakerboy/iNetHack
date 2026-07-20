334d4b65f4f6ec7a58d900f78beb40578b54360d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-20 15:19:42 -0400

    Update EngineWrapper36.h

--- nethack/include/EngineWrapper36.h
+++ nethack/include/EngineWrapper36.h
@@ -24,6 +24,7 @@ static const int NHE_ROWNO = 21;
 static const int NHE_NO_GLYPH = 5991;
 static const int NHE_WIN_INVEN = 4;
 static const int NHE_MAX_GLYPH = 5991;
+static const int NHE_BUFSZ = 256;
 
 typedef NS_ENUM(NSInteger, NHEWindowType) {
     NHEW_MESSAGE = 1,

e07c9f60a345f85faee739f9cb1cbc2b357cfdef
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-21 15:07:41 -0400

    Update method declarations in EngineWrapper36.h

--- nethack/include/EngineWrapper36.h
+++ nethack/include/EngineWrapper36.h
@@ -336,11 +336,13 @@ typedef NS_ENUM(NSInteger, NHEMenuSelectionMode) {
 
 - (BOOL)isPetGlyph:(int)glyph;
 
--(BOOL)isClickableTiles;
+- (BOOL)isClickableTiles;
 
--(BOOL)isGameOver;
+- (BOOL)isGameOver;
 
 - (char)displayInventoryWithLetters:(NSString *)letters wantReply:(BOOL)wantReply;
 
 + (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+
+- (void) setUseColorFlag:(BOOL)useColor;
 @end

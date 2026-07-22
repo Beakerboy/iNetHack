a5b02fcb9463888384d59e2c28ccbe911d1e3abd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-22 12:32:39 -0400

    Update MainViewController.h

--- Classes/MainViewController.h
+++ Classes/MainViewController.h
@@ -97,7 +97,6 @@
 
 - (void) launchNetHack;
 - (void) mainNethackLoop:(id)arg;
-- (void) mainNethackTestLoop:(id)arg;
 - (winid) createWindow:(int)type;
 - (void) destroyWindow:(winid)wid;
 - (Window *) windowWithId:(winid)wid;

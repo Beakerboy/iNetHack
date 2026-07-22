f7d012183c183d823d679b29a2706c91922e345f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-22 09:48:41 -0400

    Replace nethack_main with iphone_main in NH36EngineRunner

--- nethack/win/iphone/NH36EngineRunner.m
+++ nethack/win/iphone/NH36EngineRunner.m
@@ -2,7 +2,7 @@
 
 // Declare the external C pointer from winiphone.m
 extern id<NHWindowPortDelegate> g_window_delegate;
-extern int nethack_main(int argc, char **argv); // Your C entry point
+extern int iphone_main(int argc, char **argv); // Your C entry point
 
 @interface NH36EngineRunner : NSObject
 + (void)startEngineWithDelegate:(id<NHWindowPortDelegate>)delegate;
@@ -15,7 +15,7 @@ extern int nethack_main(int argc, char **argv); // Your C entry point
     // Kick off the NetHack C engine loop (ideally on a background thread)
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         char *argv[] = {"nethack"};
-        nethack_main(1, argv);
+        iphone_main(1, argv);
     });
 }
 @end

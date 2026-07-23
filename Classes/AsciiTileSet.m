0ffd73919cc754a5aaac56f1c6dd60d40fe6f5d1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-23 09:00:45 -0400

    Clean up imports in AsciiTileSet.m
    Removed unnecessary imports from AsciiTileSet.m

--- Classes/AsciiTileSet.m
+++ Classes/AsciiTileSet.m
@@ -19,10 +19,8 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
-#import "winiphone.h"
 #import "AsciiTileSet.h"
-#include "hack.h"
-#include "display.h"
+
 
 /*
 static float _colorTable[][4] = {

5bbc74c8e058a93d4d906bfe749b2a0ef5ff1b86
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-23 12:52:11 -0400

    Update ExtendedCommandViewController.m

--- Classes/ExtendedCommandViewController.m
+++ Classes/ExtendedCommandViewController.m
@@ -19,11 +19,8 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
-#import "winiphone.h"
 #import "ExtendedCommandViewController.h"
 #import "MainViewController.h"
-#include "hack.h"
-#include "func_tab.h"
 
 @implementation ExtendedCommandViewController
 

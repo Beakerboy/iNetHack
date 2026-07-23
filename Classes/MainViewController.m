9e7d5af4fd7a794471ba163b6eb15da008e5f59a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-23 12:55:51 -0400

    Update MainViewController.m

--- Classes/MainViewController.m
+++ Classes/MainViewController.m
@@ -21,6 +21,7 @@
 //  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "MainViewController.h"
+#import "AsciiTileSet.h"
 #import "Window.h"
 #import "MenuViewController.h"
 #import "MenuItem.h"

593cd73e1b02ee7ff06236288de1a516392201da
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-23 13:26:47 -0400

    Update NethackMenuItem.m

--- Classes/NethackMenuItem.m
+++ Classes/NethackMenuItem.m
@@ -28,7 +28,7 @@
 
 - (id) initWithId:(const void *)i title:(const char *)t glyph:(int)g isMeta:(BOOL)m preselected:(BOOL)p accelerator:(int) a {
 	if (self = [super init]) {
-		identifier = *i;
+		identifier = i;
 		if (!i->a_int) {
 			isTitle = YES;
 			children = [[NSMutableArray alloc] init];

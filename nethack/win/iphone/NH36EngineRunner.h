0af64060eeed7963bae72cfe97cb1113b269e892
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-23 08:57:01 -0400

    Create NH36EngineRunner.h

---
+++ nethack/win/iphone/NH36EngineRunner.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NH36EngineRunner : NSObject
+
+// The matching public declaration for your launch sequence
++ (UIViewController *)launchGameAndReturnViewController;
+
+@end

b449ac4b776828be5a2448c9e882905043898f31
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-24 07:32:43 -0400

    Rename NH36AsciiTileSet to AsciiTileSet

--- nethack/win/iphone/NH36AsciiTileSet.m
+++ nethack/win/iphone/NH36AsciiTileSet.m
@@ -1,8 +1,8 @@
-#import "NH36AsciiTileSet.h"
+#import "AsciiTileSet.h"
 #include "hack.h"
 #include "display.h" // Safe: compiles directly alongside 3.6 engine source code
 
-@implementation NH36AsciiTileSet
+@implementation AsciiTileSet
 
 - (NSInteger)totalGlyphsCount {
     // Returns the exact NetHack 3.6 MAX_GLYPH calculation constant

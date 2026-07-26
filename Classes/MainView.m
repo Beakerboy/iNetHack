07e696a695b7e300bb636646f0a3872351a83bbd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-26 13:31:30 -0500

    Update MainView.m

--- Classes/MainView.m
+++ Classes/MainView.m
@@ -295,7 +295,7 @@
 	center.y -= tileSize.height/2;
 
     // Check if animated tileset, and NOT rogue level.
-    if (animatedTileset && !(self.mainViewController.isRogueLevel)) {
+    if (animatedTileset && ![self.delegate mainViewShouldRenderRogueLevel) {
         int tilesetIndex = 0;
         tilesetIndex = [[MainViewController instance] animFrame];
         tileSet = tileSets[tilesetIndex];

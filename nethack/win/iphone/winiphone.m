74b54bf6fc34da0f22b6bbaeed9fc2525e027724
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-27 07:45:40 -0500

    Update winiphone.m

--- nethack/win/iphone/winiphone.m
+++ nethack/win/iphone/winiphone.m
@@ -20,14 +20,13 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
-
+#import <NetHackSharedUI/Window.h>
 #import "winiphone.h"
 
 #import <UIKit/UIKit.h>
 #import <CoreHaptics/CoreHaptics.h>
 #import <NetHackSharedUI/NHWindowPortDelegate.h>
 #import <NetHackSharedUI/MainViewController.h>
-#import <NetHackSharedUI/Window.h>
 #import <NetHackSharedUI/NethackMenuItem.h>
 #import <NetHackSharedUI/NethackYnFunction.h>
 #import <NetHackSharedUI/NethackEvent.h>

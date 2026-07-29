5e5b4cafb0fdf010431678a2516900d87986fb60
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-29 17:17:28 -0500

    Update AbstractNethackMenuViewController.h

--- Classes/AbstractNethackMenuViewController.h
+++ Classes/AbstractNethackMenuViewController.h
@@ -37,4 +37,5 @@
 }
 @property (nonatomic, assign) Window *menuWindow;
 @property (nonatomic, assign) AbstractMainViewController *mainViewController;
+@property (nonatomic, assign) ItemAmountViewController *itemAmountViewController;
 @end

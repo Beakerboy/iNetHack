32df708b2d4faacfb527adfc961c3f74ecb1d48e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-29 10:53:41 -0500

    Update AbstractNethackMenuViewController.m

--- Classes/AbstractNethackMenuViewController.m
+++ Classes/AbstractNethackMenuViewController.m
@@ -248,7 +248,7 @@ extern short glyph2tile[];
     if (i.accelerator > 0) {
         invletter = i.accelerator;
     }
-    if (showMenuLetters && menuWindow != [[MainViewController instance] windowWithId:WIN_INVEN]) {
+    if (showMenuLetters && menuWindow != [self.mainViewController windowWithId:WIN_INVEN]) {
         // If its not an inventory screen, we need to check for accelerator values instead, or else just don't show letters.
         if (i.accelerator <= 0) {
             showMenuLetters = false;

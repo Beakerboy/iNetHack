6350e665d1e1ad9bb6a5ac63581aad01ed5182cd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-30 13:54:22 -0500

    Update AbstractMainViewController.m

--- Classes/AbstractMainViewController.m
+++ Classes/AbstractMainViewController.m
@@ -769,7 +769,7 @@ static AbstractMainViewController *instance;
             return;
         } else {
             // Use monospace font if showing game ending info, the introductory text, or in wizard mode.
-            if (program_state.gameover == 1 || wizard || [message containsString:kConstIntroductoryStoryTitle]) {
+            if (self.isGameOver || wizard || [message containsString:kConstIntroductoryStoryTitle]) {
                 if ([message containsString:kConstIntroductoryStoryTitle]) {
                     // Format the intro text a bit better for narrow displays.
                     message = [self formatMessageForWideScreen:message];

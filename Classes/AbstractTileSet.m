fef7ccfae4532065afda4911cfdf634a439e4148
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-30 15:28:07 -0500

    Update and rename TileSet.m to AbstractTileSet.m

--- Classes/AbstractTileSet.m
+++ Classes/AbstractTileSet.m
@@ -20,13 +20,13 @@
 //  You should have received a copy of the GNU General Public License
 //  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
 
-#import "TileSet.h"
+#import "AbstractTileSet.h"
 
 static TileSet *instance = nil;
 
 @implementation TileSet
 
-+ (TileSet *) instance {
++ (AbstractTileSet *) instance {
 	return instance;
 }
 

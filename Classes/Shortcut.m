2693c4a709c0f3c95fc3b492aceb3539016c6454
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-31 09:14:30 -0500

    Update Shortcut.m

--- Classes/Shortcut.m
+++ Classes/Shortcut.m
@@ -21,7 +21,7 @@
 //  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "Shortcut.h"
-#import "MainViewController.h"
+#import "AbstractMainViewController.h"
 #import "NethackEventQueue.h"
 
 static NSString* ParseShortcutString (NSString* keys) {

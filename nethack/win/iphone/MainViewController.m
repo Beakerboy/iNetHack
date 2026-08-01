e7c0a0a068d2a4b40aa53e5dcc21bfafc5edcd22
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-07-31 19:09:46 -0500

    Refactor PlayerState initialization
    Removed hp and energy properties from PlayerState initialization.

--- nethack/win/iphone/MainViewController.m
+++ nethack/win/iphone/MainViewController.m
@@ -50,10 +50,6 @@ extern char lock[];
     
     state.ux        = u.ux;
     state.uy        = u.uy;
-    state.hp        = u.uhp;
-    state.hpMax     = u.uhpmax;
-    state.energy    = u.uen;
-    state.energyMax = u.uenmax;
     state.mhmax     = u.mhmax;
     state.mh        = u.mh;
     state.mtimedone = u.mtimedone;
